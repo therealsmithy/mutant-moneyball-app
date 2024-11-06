968efd33aad55b496e2a4a2d8fc76b6eb85bb40e
therealsmithy <liamsmith3434@gmail.com>
2024-11-05 20:12:01 -0500

    First working app

---
+++ app.R
@@ -0,0 +1,71 @@
+library(shiny)
+library(shinythemes)
+library(ggplot2)
+
+# Load in what is needed
+charactercounts <- readRDS('G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/charactercounts.rds')
+open <- readRDS('G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/open.rds')
+appearances <- read.csv('G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/MutantMoneyballAppearanceData.csv')
+ppiebay <- readRDS('G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/ppiebay.rds')
+ppiheritage <- readRDS('G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/ppiheritage.rds')
+ppioStreet <- readRDS('G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/ppioStreet.rds')
+ppiwiz <- readRDS('G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/ppiwiz.rds')
+
+# Define UI for the app
+ui <- fluidPage(theme = shinytheme("superhero"),
+  titlePanel("Mutant Moneyball App"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("member", "Select a Member:",
+                  choices = open$Member)
+    ),
+    mainPanel(
+      plotOutput("scatterplot1"),
+      plotOutput("scatterplot2"),
+      plotOutput("scatterplot3"),
+      plotOutput("scatterplot4")
+    )
+  )
+)
+
+# Define server logic for app
+server <- function(input, output) {
+  output$scatterplot1 <- renderPlot({
+    ggplot(ppiebay[ppiebay$Member == input$member,], 
+           aes(x = Decade, y = TotalValue_eBay)) +
+      geom_point() +
+      geom_line(group = 1) +
+      labs(title = paste("PPI for", input$member),
+           x = "Date",
+           y = "PPI")
+  })
+  output$scatterplot2 <- renderPlot({
+    ggplot(ppiheritage[ppiheritage$Member == input$member,],
+           aes(x = Decade, y = TotalValue_Heritage)) +
+      geom_point() +
+      geom_line(group = 1) +
+      labs(title = paste("PPI for", input$member),
+           x = "Date",
+           y = "PPI")
+  })
+  output$scatterplot3 <- renderPlot({
+    ggplot(ppiwiz[ppiwiz$Member == input$member,],
+           aes(x = Decade, y = TotalValue_Wiz)) +
+      geom_point() +
+      geom_line(group = 1) +
+      labs(title = paste("PPI for", input$member),
+           x = "Date",
+           y = "PPI")
+  })
+  output$scatterplot4 <- renderPlot({
+    ggplot(ppioStreet[ppioStreet$Member == input$member,],
+           aes(x = Decade, y = TotalValue_oStreet)) +
+      geom_point() +
+      geom_line(group = 1) +
+      labs(title = paste("PPI for", input$member),
+           x = "Date",
+           y = "PPI")
+  })
+}
+
+shinyApp(ui = ui, server = server)
