49295f8fc0123d8f485784f1b281deca880ad178
therealsmithy <liamsmith3434@gmail.com>
2024-11-05 19:45:36 -0500

    More wrangling for easier plots

--- wrangling.R
+++ wrangling.R
@@ -57,6 +57,39 @@ open$Member <- gsub('Remy Le Beau', 'Remy LeBeau', open$Member)
 # Convert columns 23 through 46 to numeric in open after cleaning them
 open[, 23:46] <- lapply(open[, 23:46], function(x) as.numeric(gsub('[^0-9]', '', x)))
 
+# Make it so PPI60s_heritage, PPI70s_heritage, PPI80s_heritage, PPI90s_heritage can be graphed as a time series
+library(dplyr)
+library(tidyr)
+ppiheritage <- open %>% 
+                 pivot_longer(cols = c(PPI60s_heritage, PPI70s_heritage, PPI80s_heritage, PPI90s_heritage),
+                           names_to = 'Decade', values_to = 'TotalValue_Heritage') %>% 
+                 mutate(Decade = gsub('PPI', '', Decade)) %>% 
+                 mutate(Decade = gsub('s_heritage', '', Decade)) %>% 
+                 select(Member, Alias, Decade, TotalValue_Heritage)
+
+# Do that for the other three PPI types
+ppiebay <- open %>% 
+            pivot_longer(cols = c(PPI60s_ebay, PPI70s_ebay, PPI80s_ebay, PPI90s_ebay),
+                      names_to = 'Decade', values_to = 'TotalValue_eBay') %>% 
+            mutate(Decade = gsub('PPI', '', Decade)) %>% 
+            mutate(Decade = gsub('s_ebay', '', Decade)) %>% 
+            select(Member, Alias, Decade, TotalValue_eBay)
+
+ppiwiz <- open %>%
+           pivot_longer(cols = c(PPI60s_wiz, PPI70s_wiz, PPI80s_wiz, PPI90s_wiz),
+                     names_to = 'Decade', values_to = 'TotalValue_Wiz') %>% 
+           mutate(Decade = gsub('PPI', '', Decade)) %>% 
+           mutate(Decade = gsub('s_wiz', '', Decade)) %>% 
+           select(Member, Alias, Decade, TotalValue_Wiz)
+
+ppiostreet <- open %>% 
+               pivot_longer(cols = c(PPI60s_oStreet, PPI70s_oStreet, PPI80s_oStreet, PPI90s_oStreet),
+                         names_to = 'Decade', values_to = 'TotalValue_oStreet') %>% 
+               mutate(Decade = gsub('PPI', '', Decade)) %>% 
+               mutate(Decade = gsub('s_oStreet', '', Decade)) %>% 
+               select(Member, Alias, Decade, TotalValue_oStreet)
+
+
 # Exploratory Vizzes
 library(ggplot2)
 library(ggdark)
@@ -74,3 +107,7 @@ ggplot(top10appearances, aes(x = numIssues, y = character_name, fill = color_cod
 # Save for use in other files
 #saveRDS(open, 'G:/My Drive/R Projects/mutant-moneyball-app/data/open.rds')
 #saveRDS(charactercounts, 'G:/My Drive/R Projects/mutant-moneyball-app/data/charactercounts.rds')
+#saveRDS(ppiebay, 'G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/ppiebay.rds')
+#saveRDS(ppiheritage, 'G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/ppiheritage.rds')
+#saveRDS(ppiwiz, 'G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/ppiwiz.rds')
+#saveRDS(ppiostreet, 'G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/ppiostreet.rds')
