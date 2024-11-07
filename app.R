6d9812a50df84ffea54062977b3b5ff9affb8c80
therealsmithy <liamsmith3434@gmail.com>
2024-11-06 19:29:58 -0500

    App updates

--- app.R
+++ app.R
@@ -10,11 +10,16 @@ ppiebay <- readRDS('G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball
 ppiheritage <- readRDS('G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/ppiheritage.rds')
 ppioStreet <- readRDS('G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/ppioStreet.rds')
 ppiwiz <- readRDS('G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/ppiwiz.rds')
+appearancepercentages <- readRDS('G:/My Drive/R Projects/mutant-moneyball-app/mutant-moneyball-app/data/appearancepercentages.rds')
 
 # Define UI for the app
-# The UI should have two rows of two graphs
 ui <- fluidPage(theme = shinytheme("superhero"),
   titlePanel("Mutant Moneyball App"),
+  tags$style(HTML('
+      .big-text {
+        font-size: 50px;
+    }
+                  ')),
   sidebarLayout(
     sidebarPanel(
       selectInput("member", "Select a Member:",
@@ -22,54 +27,45 @@ ui <- fluidPage(theme = shinytheme("superhero"),
     ),
     mainPanel(
           fluidRow(
-            column(6, plotOutput("scatterplot1")),
-            column(6, plotOutput("scatterplot2"))
+            column(6, uiOutput("numissues")),
+            column(6, uiOutput("totalvalue")
           ),
           fluidRow(
-            column(6, plotOutput("scatterplot3")),
-            column(6, plotOutput("scatterplot4"))
+            column(6, plotOutput("scatterplot1")),
+            column(6, plotOutput("scatterplot2"))
           )
     )
   )
-)
+))
 
 # Define server logic for app
 server <- function(input, output) {
-  output$scatterplot1 <- renderPlot({
-    ggplot(ppiebay[ppiebay$Member == input$member,], 
-           aes(x = Decade, y = TotalValue_eBay)) +
-      geom_point() +
-      geom_line(group = 1) +
-      labs(title = paste("PPI for", input$member),
-           x = "Date",
-           y = "PPI")
+  output$numissues <- renderUI({
+    div(class = 'big-text',paste("Number of Issues:", open$TotalIssues[open$Member == input$member]))
   })
+  
+  output$totalvalue <- renderUI({
+    div(class = 'big-text' ,paste("Total Value: $", prettyNum(open$TotalValue_heritage[open$Member == input$member]), big.mark = ","))
+  })
+  
   output$scatterplot2 <- renderPlot({
-    ggplot(ppiheritage[ppiheritage$Member == input$member,],
+    ggplot(ppiheritage[ppiheritage$Member == input$member,], 
            aes(x = Decade, y = TotalValue_Heritage)) +
       geom_point() +
       geom_line(group = 1) +
       labs(title = paste("PPI for", input$member),
-           x = "Date",
+           x = "Decade",
            y = "PPI")
   })
-  output$scatterplot3 <- renderPlot({
-    ggplot(ppiwiz[ppiwiz$Member == input$member,],
-           aes(x = Decade, y = TotalValue_Wiz)) +
+  
+  output$scatterplot1 <- renderPlot({
+    ggplot(appearancepercentages[appearancepercentages$Member == input$member,], 
+           aes(x = Decade, y = Appearance_Percent)) +
       geom_point() +
       geom_line(group = 1) +
-      labs(title = paste("PPI for", input$member),
-           x = "Date",
-           y = "PPI")
-  })
-  output$scatterplot4 <- renderPlot({
-    ggplot(ppioStreet[ppioStreet$Member == input$member,],
-           aes(x = Decade, y = TotalValue_oStreet)) +
-      geom_point() +
-      geom_line(group = 1) +
-      labs(title = paste("PPI for", input$member),
-           x = "Date",
-           y = "PPI")
+      labs(title = paste("Appearance Percentages for", input$member),
+           x = "Decade",
+           y = "Percentage")
   })
 }
 
